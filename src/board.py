f553944803b6f861b2f3d64ccd90c88655fcc78d
Rami Ferjani <ramiferjani.20@gmail.com>
2025-01-30 23:13:03 +0000

    Add files via upload

--- src/board.py
+++ src/board.py
@@ -1,14 +1,73 @@
 from const import *
 from square import Square
 from piece import *
+from move import  *
 
 
 class Board:
     def __init__(self):
         self.squares = [[None for _ in range(cols)] for _ in range(rows)]
+
         self._create()
         self._add_pieces('white')
         self._add_pieces('black')
+
+    def calc_moves(self, piece, row, col):
+        '''
+
+        :param piece:
+        :param row:
+        :param col:
+        :return:  calculate all the possible (valid ) moves of an specific piece ona a specific position
+
+        '''
+
+        def knight_moves():
+            # 8 possible moves
+            possible_moves = [
+                (row - 2, col + 1),
+                (row - 1, col + 2),
+                (row + 1, col + 2),
+                (row + 2, col + 1),
+                (row + 2, col - 1),
+                (row + 1, col - 2),
+                (row - 1, col - 2),
+                (row - 2, col - 1),
+
+            ]
+            for possible_moves in possible_moves:
+                possible_moves_row, possible_moves_col = possible_moves
+                if Square.in_range(possible_moves_row, possible_moves_col):
+                    if self.squares[possible_moves_row][possible_moves_col].isempty_or_rival(piece.color):
+                        # create squares of the new move
+                        initial = Square(row , col )
+                        final = Square(possible_moves_row , possible_moves_col ) #piece = piece
+                        # create new moves
+                        move = Move(initial , final)
+                        # append new bool move
+                        pass
+
+        if isinstance(piece, Pawn):
+            pass
+
+        elif isinstance(piece, Knight):
+            knight_moves()
+
+
+        elif isinstance(piece, Rook):
+            pass
+
+
+        elif isinstance(piece, Queen):
+            pass
+
+
+        elif isinstance(piece, King):
+            pass
+
+
+        elif isinstance(piece, Bishop):
+            pass
 
     # we put '_' after the name of the function to indicate that these methods are private
     def _create(self):

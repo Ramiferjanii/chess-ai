c6058f55b51bb32c7ac92715480b38b67378b734
Rami Ferjani <ramiferjani.20@gmail.com>
2025-02-03 17:59:54 +0000

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,17 @@
+
+ class  Config :
+     def __init__(self):
+         pass
+
+
+
+
+     def change_theme(self):
+         pass
+
+
+
+
+
+     def _add_themes(self):
+         pass
---
+++ src/theme.py
@@ -0,0 +1,10 @@
+class Theme :
+
+
+
+    def __init__(self):
+        pass
+
+
+
+
